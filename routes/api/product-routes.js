3ac663aababf5d990355ebd98662cf03b1ca0d8c
bwax <braxton.waxdeck@gmail.com>
2023-01-16 20:43:53 -0800

    sequelized it, started routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,16 +3,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
+
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll();
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  console.log("req params", req.params.id);
+  Product.findByPk(req.params.id)
+    .then(product => {
+      if (!product) {
+        return res.status(404).json({ message: 'Product not found' });
+      }
+      res.json(product);
+    })
+    .catch(err => res.status(400).json(err));
 });
 
 // create new product
